5a460004cc77b57fc2f1a6d2381325bbaa1fbdb2
Estilicious <81353867+Estilicious@users.noreply.github.com>
2022-04-04 16:22:12 +0900

    Initialize project using Create React App and deleted unwanted items

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="esylogo.png" className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
