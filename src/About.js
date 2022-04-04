6db0ba53c886c10971184a68a848473df768ab66
Estilicious <81353867+Estilicious@users.noreply.github.com>
2022-04-04 18:47:15 +0900

    Added logo, updated css and created the about page.

---
+++ src/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function About(){
+    return (
+<div className="about-container">
+    <div className="row">
+        
+        <div className="col-6">
+<img src="esypic2.png" className="img-fluid rounded-circle border border-warning" alt="A visual representation of the portfolio's owner" rel="noreferrer"/> 
+        </div>
+        <div className= "col-6">
+    <h2>
+        About Me
+    </h2>
+    <p>
+       Hello! I'm a certified language teacher, multilingual and junior front-end developer currently living in Japan. My coding journey was born out of a desire to switch to a fast-paced career.
+<br/> After Several trial and error in random coding bootcamps in 2020, I thought coding wasn't for me. In February 2021, I decided to give it one last shot and enrolled at SheCodes. Despite the complexities that come with coding, I'm determined to ride the waves until I sail at long last in the sea of web development.
+<br/>
+I'm currently seeking new opportunities in front-end development. I'm open to work remotely and have flexible hours. I'm skilled in Javascript, CSS, Flebox, Bootstrap, VanillaJS, SEO and React. I'm currently learning Ruby. 
+    </p>
+    </div>
+    </div>
+</div>
+    );
+}
--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import About from "./About.js"
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="esylogo.png" className="App-logo" alt="logo" />
-      </header>
+    <div className="App-intro">
+        <img src="esylogo1.png" className="App-logo" alt="logo" />
+      <h2>
+        Hi there! I'm
+      </h2>
+      <h1>
+        Esther Chiamaka Chukwu
+      </h1>
+      <h3>
+Junior Web Developer
+      </h3>
+      <br/>
+      <About />
     </div>
   );
 }
+
+<About />
